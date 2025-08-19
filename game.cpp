ec93204130133fc19b0140eee9a017a3557aa1c3
DuckWuynh <emkochoigame@gmail.com>
2025-08-19 07:31:37 +0700

    fix pipe, nhưng chưa ổn

--- game.cpp
+++ game.cpp
@@ -17,19 +17,34 @@ int main() {
 
     Sprite background (backgroundTexture); // tạo sprite từ texture nền
     
-
+    /*==========================================================================*/
+    
     // Tạo ống :>>
     Texture pipeTexture; // tạo texture cho ống
     if (!pipeTexture.loadFromFile("ong.png")) { // tải hình ảnh ống từ file
         return -1; // nếu không tải được, trả về -1 để kết thúc chương trình
     }
+    // Khởi tạo các biến cho ống
+    int goundHeight = 100; // chiều cao của mặt đất, có thể thay đổi để phù hợp với hình nền
+    int pipeX= 400; // vị trí ống theo trục X, có thể thay đổi để di chuyển ống
+    int pipeY = 0; // vị trí ống theo trục Y, sẽ được tính toán dựa trên khoảng cách giữa ống trên và ống dưới
+    int pipeTopY = -50; // vị trí ống trên, có thể thay đổi để tạo độ khó
+    int khoangcach=150; // sẽ là khoảng cách giữa ống trên và ống dưới, có thể thay đổi để tạo độ khó
+    int gap = khoangcach; // dùng biến tạm sao chép khoảng cách giữa ống trên và ống dưới
+    int pipeHeight = pipeTexture.getSize().y; // lấy chiều cao của ống từ texture
+    int pipeWidth = pipeTexture.getSize().x; // lấy chiều rộng của ống từ texture
+
+    // Ống trên
     Sprite pipeTop(pipeTexture); // tạo sprite từ texture ống
-    pipeTop.setPosition(400, 0); // vị trí của ống trên cùng
+    //pipeTop.setPosition(400, 300); // vị trí của ống trên cùng, mặc dù vẽ ống trên cùng nhưng chả hiểu sao nó lại là cái ống bển dưới =))
+    pipeTop.setOrigin(pipeWidth / 2, 0); // đặt gốc của ống trên tại giữa cạnh trên của nó
+    pipeTop.setPosition(pipeX,pipeTopY); // vị trí của ống trên, nó sẽ nằm ở trên cùng của cửa sổ
     // ống dưới sẽ được tạo bằng cách lật ngược ống trên
     Sprite pipeBottom(pipeTexture); // tạo sprite từ texture ống
-    pipeBottom.setPosition(400, 400); // vị trí của ống dưới
+    pipeBottom.setOrigin(pipeWidth / 2, pipeHeight); // đặt gốc của ống dưới tại giữa cạnh dưới của nó
+    //pipeBottom.setPosition(400, 600); // vị trí của ống dưới, nó lại là cái ống bển trên =))
     pipeBottom.setRotation(180); // lật ngược ống dưới
-
+    pipeBottom.setPosition(pipeX, pipeHeight + gap + pipeTopY - 100); // vị trí của ống dưới, nó sẽ nằm ở dưới ống trên với khoảng cách đã định
 
 
     // Tạo chim :>>
